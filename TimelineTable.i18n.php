2f3c81c9fa102e5840a2dd5a342b76939d5bf7da
thibaultmarin <thibaultmarin@localhost>
2012-07-01 07:44:45 +0000

    TimelineTable tag extension: first check-in.

---
+++ TimelineTable.i18n.php
@@ -0,0 +1,32 @@
+<?php
+
+$messages = array();
+$messages['en'] = array(
+	'january' => 'January',
+	'february' => 'February',
+	'march' => 'March',
+	'april' => 'April',
+	'may' => 'May',
+	'june' => 'June',
+	'july' => 'July',
+	'august' => 'August',
+	'september' => 'September',
+	'october' => 'October',
+	'november' => 'November',
+	'december' => 'December',
+);
+
+$messages['fr'] = array(
+	'january' => 'Janvier',
+	'february' => 'Fevrier',
+	'march' => 'Mars',
+	'april' => 'Avril',
+	'may' => 'Mai',
+	'june' => 'Juin',
+	'july' => 'Juilet',
+	'august' => 'Aout',
+	'september' => 'Septembre',
+	'october' => 'Octobre',
+	'november' => 'Novembre',
+	'december' => 'Decembre',
+);
---
+++ TimelineTable.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * TimelineTable - this extension creates a timeline contained in a simple HTML
+ * table.
+ *
+ * To activate this extension, add the following into your LocalSettings.php file:
+ * require_once('$IP/extensions/timelinetable.php');
+ *
+ * @ingroup Extensions
+ * @author Thibault Marin
+ * @version 1.7.1
+ * @link http://www.mediawiki.org/wiki/Extension:TimelineTable
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ *
+ * @revision
+ * 1.7 -> 1.7.1
+ *  Rename tag to avoid conflict
+ * 1.6 -> 1.7
+ *  Fix bug in months processing (when using only years).
+ * 1.5 -> 1.6
+ *  Support for timeline using years only.
+ *  Deprecated split() function replaced by explode().
+ * 1.0 -> 1.5
+ *  Added ability to display days level.
+ *  Reviewed code.
+ */
+
+/**
+ * Protect against register_globals vulnerabilities.
+ * This line must be present before any global variable is referenced.
+ */
+if( !defined( 'MEDIAWIKI' ) )
+{
+	echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
+	die( -1 );
+}
+
+// Extension credits that will show up on Special:Version
+$wgExtensionCredits['validextensionclass'][] = array(
+	'name'         => __FILE__,
+	'version'      => '1.7.1',
+	'author'       => 'Thibault Marin',
+	'url'          => 'http://www.mediawiki.org/wiki/Extension:TimelineTable',
+	'description'  => 'Create a table containing a timeline'
+);
+
+//Avoid unstubbing $wgParser on setHook() too early on modern (1.12+) MW
+//versions, as per r35980
+if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
+	$wgHooks['ParserFirstCallInit'][] = 'efTimelineTableParserInit';
+	$wgHooks['ParserAfterTidy'][]='efTimelineTableAfterTidy';
+	$wgExtensionFunctions[] = 'efTimelineTableParserInit';
+} else { // Otherwise do things the old fashioned way
+	$wgHooks['ParserAfterTidy'][]='efTimelineTableAfterTidy';
+	$wgExtensionFunctions[] = 'efTimelineTableParserInit';
+}
+
+function efTimelineTableParserInit() {
+	global $wgParser;
+	$wgParser->setHook( 'timelinetable', 'efTimelineTableRender' );
+	return true;
+}
+
+$wgExtensionMessagesFiles['TimelineTable'] = dirname( __FILE__ ) . '/TimelineTable.i18n.php';
+require_once dirname(__FILE__) . '/TimelineTable.body.php';
+
